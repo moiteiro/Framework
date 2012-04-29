5ce2bd702f40e270afada8160ae501d69a3807c0
Moiteiro <bruno.moiteiro@gmail.com>
2012-04-29 16:06:17 -0300

    modificando o nome de algumas funcoes e inserindo Exceptions no lugar dos echos's

--- libs/database.php
+++ libs/database.php
@@ -5,7 +5,7 @@
 * A conexao eh estabalecida no momento da instaciacao
 * 
 * @author Bruno Moiteiro <bruno.moiteiro@gmail.com>
-* @version 1.1
+* @version 1.2
 * @copyright Copyright (c) 2011, Bruno Moiteiro
 */
  
@@ -39,8 +39,7 @@ class MySQLDatabase{
 	* Fecha a conexão com o banco de dados
 	* No momento em que a variável for destruida ele fechará a conexão
 	* @return void
-	*/
-	
+	*/	
 	public function __destruct(){
 		$this->close_connection();	
 	}
@@ -56,11 +55,11 @@ class MySQLDatabase{
 	protected function open_connection(){
 		$this->connection = mysql_connect(HOST, USERNAME,PASSWORD);
 		if (!$this->connection){
-			echo "Database connection failed :". mysql_error();	
+			throw new Exception("Database connection failed :". mysql_error(), 1);	
 		} else {
 			$selected = mysql_select_db(DATABASE, $this->connection);
 			if (!$selected){
-				echo "Database selection failed: ". mysql_error();
+				throw new Exception("Database selection failed: ". mysql_error(), 1);
 			}
 		}
 	}
@@ -70,7 +69,6 @@ class MySQLDatabase{
 	* a função termina a conexão com a função mysql_close e limpa a variável {@link $connection}
 	* @return void
 	*/ 
-
 	protected function close_connection(){
 		if($this->connection){
 			mysql_close($this->connection);
@@ -86,7 +84,6 @@ class MySQLDatabase{
 	* @param string $sql
 	* @return resource
 	*/
-
 	public function query($sql){
 		$this->last_query = $sql;
 		$result = mysql_query ($sql);
@@ -98,8 +95,7 @@ class MySQLDatabase{
 	* Transforma o resultado o resource gerado por {@link query()} em array
 	* @param resource $result
 	* @return array
-	*/
-	
+	*/	
 	public function fetch_array($result){
 		$result_set = mysql_fetch_array($result);
 		return $result_set;
@@ -109,18 +105,16 @@ class MySQLDatabase{
 	* Escapa os caracteres especiais numa string para usar em uma instrução SQL
 	* @param string $string
 	* @return string
-	*/
-	
-	public function escape_string($string){
+	*/	
+	public function escape_value($string){
 		return mysql_real_escape_string($string);	
 	}
-	
+
 	/**
 	* Verifica a quantidade de linhas no banco de dados que de acordo com o resource gerado por {@link query()} 
 	* @param resource $result
 	* @return int
-	*/
-	
+	*/	
 	public function num_rows($result){
 		$amount = mysql_num_rows($result);
 		return $amount;
@@ -131,8 +125,7 @@ class MySQLDatabase{
 	* Através da ultima inserção no banco de dados, essa função retorna a última linha inserida no banco que contenha id
 	* @return int
 	*/
-	
-	public function last_id(){
+	public function insert_id(){
 		return mysql_insert_id($this->connection);	
 	}
 	
@@ -140,17 +133,32 @@ class MySQLDatabase{
 	* Verifica a quantidade de linhas que foram afetadas com a última query gerada por {@link query()} 
 	* @return int
 	*/
-	
 	public function affected_rows(){
 		return mysql_affected_rows($this->connection);	
 	}
 	
+
+	/**
+	 * Retorna a quantidade de valores encontrados depois de aplicar algum filtro.
+	 * Essa funcao deve ser chamada apos utlidade SLQ_CALC_FOUND_ROWS() (mysql command);
+	 * @access public
+	 * @return integer
+	 */
+	public function found_rows(){
+
+		$sql = "SELECT FOUND_ROWS() AS total";
+		$this->last_query = $sql;
+		$result_set = mysql_query ($sql);
+		$this->confirm_query($result_set);
+		$result = $this->fetch_array($result_set);
+		return $result['total'];
+	}
+
 	/**
 	* Transforma o Resource gerado por {@link query()} em um array onde cada posição representa uma linha do banco
 	* @param resource $resource
 	* @return array
 	*/
-	
 	public function result_to_array($resource){
 		$result_array = array();
 		for ($i=0; $row = $this->fetch_array($resource); $i++){
@@ -163,12 +171,12 @@ class MySQLDatabase{
 	* Verifica se o resultado de uma query ao banco do dados foi realizado com sucesso.
 	* Caso algum problema tenha ocorrido, uma mensagem será exibida na tela.
 	* @return void
-	*/
-	
+	*/	
 	protected function confirm_query($result){
 		if (!$result){
-			echo "Database query failed: ". mysql_error() . "<br /><br />";
-			echo "Last SQL query: ". $this->last_query;
+			$msg = "Database query failed: ". mysql_error() . "<br /><br />";
+			$msg = "Last SQL query: ". $this->last_query;
+			throw new Exception($msg, 1);
 		}
 	}
 	
