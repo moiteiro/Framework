2af89cee2ae41cfecc7d1784a06bce42051d0bb6
Moiteiro <bruno.moiteiro@gmail.com>
2012-05-14 23:04:26 -0300

    Remodelando a estrutura do framework. Colocando scritps de mais baixo nivel em pasta diferente

---
+++ core/init.php
@@ -0,0 +1,14 @@
+<?php 
+
+session_start();
+
+require_once('routes.php');
+require_once('core_functions.php');
+require_once('/includes/routes.php');
+require_once('/includes/config.php');
+// Definindo funcoes customizadas para serem exibidas caso haja uma Exception, Notice ou Warning.
+set_exception_handler('dflt_exception_handler');
+set_error_handler('dflt_error_handler');
+
+
+$route = new Route($routes);
