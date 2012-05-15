70560404cc39eaf56ae87c7a516a39032de0155b
Moiteiro <bruno.moiteiro@gmail.com>
2012-05-14 23:00:44 -0300

    padronizando a rota nos formularios

--- views/example/new.php
+++ views/example/new.php
@@ -1,4 +1,4 @@
-<form action="<?php echo WEBSITE.DS.'ventures'.DS.'create' ;?>" 
+<form action="<?php echo WEBSITE.DS.$route->controller.DS.'create' ;?>" 
     id="form-create-user" 
     class="block-content form" 
     enctype="multipart/form-data" 
