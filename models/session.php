a6109a9f6e00120e419575b7f518f3937060ad94
Moiteiro <bruno.moiteiro@gmail.com>
2012-09-15 23:43:37 -0300

    Removendo da class Session a autenticacao do usuario.

--- models/session.php
+++ models/session.php
@@ -1,45 +1,25 @@
 <?php 
 
-// prevent the direct access
-defined('_PREVENT-DIRECT-ACCESS') or die ("Access restrict");
-
-class Session{
+class Session extends DatabaseObject{
 	
-	public function login($params){
-		global $database;
+	public function login($user){
 		
-		$query = sprintf("SELECT * FROM users WHERE 
-												 username = '%s' 
-											 AND 
-												 password = '%s' 
-											 LIMIT 1",
-							mysql_real_escape_string($params['username']),
-							mysql_real_escape_string(md5($params['password'])));
-													  
-		$result = $database->query($query);
-		
-		if($database->num_rows($result)){
-			
-			$user = $database->fetch_array($result);
-			$this->set_session($user);
-			
-		}
+		self::create_session($user);
 	}
 	
 	public function logout(){
-		$this->unset_session();
+		$this->destroy_session();
 	}
 	
 	public function is_logged(){
 		return (isset($_SESSION['user']) ? true:false);	
 	}
 	
-	protected function set_session($user){
-		$_SESSION['user']['username'] 	= $user['username'];
-		$_SESSION['user']['id']			= $user['id'];
+	protected function create_session($user){
+		$_SESSION['user']['id']			= $user->id;
 	}
 	
-	protected function unset_session(){
+	protected function destroy_session(){
 		if(isset($_SESSION['user'])){
 			unset($_SESSION['user']);
 		}
