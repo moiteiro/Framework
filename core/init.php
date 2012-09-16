adf54a331110434c7a0c770d1b00dd365b8d62ba
Moiteiro <bruno.moiteiro@gmail.com>
2012-09-15 23:15:34 -0300

    Adicionado o carregamento dos arquivo 'core' do sistema, geracao e de rotas e de menu.

--- core/init.php
+++ core/init.php
@@ -2,13 +2,52 @@
 
 session_start();
 
+
+/* ================================
+	Including Abstract Classes 
+===================================*/
+require_once('classes/file.php');
+
+/* ===============================
+	Including core funcitions
+==================================*/
 require_once('routes.php');
+require_once('dictionary.php');
 require_once('core_functions.php');
-require_once('/includes/routes.php');
-require_once('/includes/config.php');
-// Definindo funcoes customizadas para serem exibidas caso haja uma Exception, Notice ou Warning.
+
+/* ==========================
+	System Definitions
+============================*/
 set_exception_handler('dflt_exception_handler');
 set_error_handler('dflt_error_handler');
 
+/* ============
+	Configs
+===============*/
+require_once($_SERVER['DOCUMENT_ROOT'].'/includes/config.php');
+require_once(INCLUDES_PATH.DS.'routes.php');
 
-$route = new System\Route($routes);
+
+
+/* ================================================
+================================================ */
+$route_app = new System\Route();
+$route_app->set_file_path(ROUTES_PATH);
+
+
+/* ===================================
+	System Operation Mode 
+	(Development|Production)
+======================================*/
+if(OPERATIONAL_MODE == 'production'){
+	
+} else {
+
+	$route_app->generate($route_apps);
+	$route_app->generate_xml();
+	$route_app->export_as_menu();
+}
+
+
+// carregando o dicionário.
+$dictionary = new System\Dictionary(DICTIONARY_PATH);
